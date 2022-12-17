45bd9804823749eba3b90d432278aeea8bc4391d
Juliana <juliana.hooson16@gmail.com>
2022-12-16 17:32:16 -0800

    Final Push

--- julesPage.ts
+++ julesPage.ts
@@ -2,5 +2,30 @@ import {BasePage} from './basePage'
 import{By} from 'selenium-webdriver'
 
 export class Jules extends BasePage {
-    
+    homePage: By = By.xpath('(//a[@class="preSlide slideIn"])[2]')
+    shopPage: By = By.xpath('(//a[@class="preSlide slideIn"])[4]')
+    succulentArrrangment: By = By.xpath('(//a[@class="grid-item-link"])[1]')
+    shippedGiftCard: By = By.xpath('(//a[@class="grid-item-link"])[2]')
+    succulentKit: By = By.xpath('(//a[@class="grid-item-link"])[3]')
+    dropdownColor: By = By.xpath('(//div[@class="variant-select-wrapper"])[1]')
+    selectPeach: By = By.xpath('(//option[text()="Peach"])[1]')
+    selectSize: By = By.xpath('(//div[@class="variant-select-wrapper"])[2]')
+    selectMedium:By = By.xpath('(//option[text()="Medium"])[1]')
+    purchaseButton: By = By.xpath('(//div[@class="sqs-add-to-cart-button sqs-suppress-edit-mode sqs-button-element--primary"])[1]')
+    instagram: By = By.xpath('(//a[@class="icon icon--fill icon--sm  header-icon header-icon-border-shape-none header-icon-border-style-outline"])[3]')
+    pintrest: By = By.xpath('(//a[@class="icon icon--fill icon--sm  header-icon header-icon-border-shape-none header-icon-border-style-outline"])[2]')
+    facebook: By = By.xpath('(//a[@class="icon icon--fill icon--sm  header-icon header-icon-border-shape-none header-icon-border-style-outline"])[1]')
+
+    constructor(){
+        super({
+            url:"https://thesucculentshop.com/"
+        })
+    }
+    async tabs() {
+        let myTabs = await this.driver.getAllWindowHandles()
+        await this.driver.switchTo().window(myTabs[1]);
+        await this.driver.sleep(1000);
+        await this.driver.close()
+        await this.driver.switchTo().window(myTabs[0])
+    } 
 }
--- julesTest.test.ts
+++ julesTest.test.ts
@@ -1,2 +1,43 @@
 import {Jules} from './julesPage'
 const jules = new Jules()
+const fs=require ('fs')
+
+describe('Jules Solo Project', () => {
+    test('Confirming the shop page', async () => {
+       await jules.navigate()
+       await jules.click(jules.shopPage) 
+       await jules.click(jules.succulentArrrangment)
+       await jules.click(jules.shopPage)
+       await jules.click(jules.shippedGiftCard) 
+       await jules.click(jules.shopPage)
+       await jules.click(jules.succulentKit)
+       await jules.click(jules.homePage)
+    })
+    test('Social Media Test', async ()=> {
+        await jules.navigate()
+        await jules.click(jules.instagram)
+        await jules.tabs()
+        await jules.click(jules.pintrest)
+        await jules.tabs()
+        await jules.click(jules.facebook)
+        await jules.tabs()
+     })
+     test('Add to Cart', async ()=> {
+        await jules.navigate()
+        await jules.click(jules.shopPage)
+        await jules.click(jules.succulentArrrangment)
+        await jules.click(jules.dropdownColor)
+        await jules.click(jules.selectPeach)
+        await jules.click(jules.selectSize)
+        await jules.click(jules.selectMedium)
+        await jules.click(jules.purchaseButton)
+        await jules.driver.sleep(5000)
+        await fs.writeFile(`${__dirname}/checkout.png`,
+        await jules.driver.takeScreenshot(), "base64",
+        (e) => {
+            if (e) console.error(e)
+            else console.log("Save Successful")
+        })
+        await jules.driver.quit() 
+     })
+})
